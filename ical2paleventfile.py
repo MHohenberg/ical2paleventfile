df3df735df0194cce8d533578574a6bdded1eb1f
Martin Hohenberg <martin.hohenberg@gmail.com>
2017-08-06 10:31:38 +0200

    Changed to sane fileendings, added start time of event

--- ical2paleventfile.py
+++ ical2paleventfile.py
@@ -4,51 +4,55 @@ from ics import Calendar
 from os.path import expanduser
 from urllib2 import urlopen
 from ConfigParser import SafeConfigParser
+import datetime as dt
+from datetime import datetime
+from dateutil import tz
 
 homedir = expanduser("~")
 
-config = homedir+'/.ical2paleventfile/calendars'
+config_file = homedir+'/.ical2paleventfile/calendars'
 
 parser = SafeConfigParser()
-parser.read(config)
+parser.read(config_file)
 
 for section in parser.sections():
     
     url = parser.get(section, "url")
-    output = parser.get(section, "palname")
+    pal_file = homedir+"/.pal/"+parser.get(section, "palname")
     calendarname = parser.get(section, "shorthand")+" "+parser.get(section, "name")
 
     print "url: "+url
-    print "pal: "+output
+    print "pal: "+pal_file
     print "calendar name: "+calendarname
 
     c = Calendar(urlopen(url).read().decode('utf-8'))
 
-    f = open(output, 'w')
+    f = open(pal_file, 'w')
     f.write(calendarname+"\n")
 
-    errorcounter = 0
-
     for event in c.events:
-
+        
         try:
             name = event.name
             if (name.isspace() or len(name) == 0):
                 name = "[Event without title]"
-
-            beginstring = str(event.begin).replace('-','')[:8]
-            endstring = beginstring
-            if (event.has_end):
-                endstring = str(event.end).replace('-', '')[:8]
-    
-            if (beginstring == endstring):
-                f.write(beginstring+" "+ name+"\n")
+            
+            begin_date_local = event.begin.astimezone(tz.tzlocal())
+            
+            begin_date = str(begin_date_local).replace('-','')[:8]
+            begin_time = str(begin_date_local).replace('-','')[9:14]
+	    end_date = begin_date
+            
+	    if (event.has_end):
+                end_date = str(event.end).replace('-', '')[:8]
+                
+            if (begin_date == end_date):
+                f.write(begin_date+" ["+begin_time+"] "+ name+"\n")
             else:
-                f.write("DAILY:"+beginstring+":"+endstring+" "+name+"\n")
+                f.write("DAILY:"+begin_date+":"+end_date+" "+name+"\n")
 
         except UnicodeEncodeError:
-            errorcounter = errorcounter+1
-            print "Bloop "+str(errorcounter)
-
+            print "UnicodeEncodeError"
+    
     f.close()
 
