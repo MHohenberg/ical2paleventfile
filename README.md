b8f6b088eb6b4662302638cfe6005ed8c2ef9a6a
Martin Hohenberg <martin.hohenberg@gmail.com>
2017-08-06 09:20:48 +0200

    Read from config file, allow for multiple calendars

--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 I love pal. Pal is a command-line calendar on unixoid systems. 
 
 This little script converts an ical to a pal-compatible file
+
+## Requirements
+
+* ics 0.31 or later (pip install ics)
+
+## configuration
+
+ical2paleventfile reads the config file ~/.ical2paleventfile/calendars 
+
+You can add as many calendars as you want
+
+    [calendar0815]   # Make sure the section name is different for every calendar
+    url = [URL of the ICS file]
+    palname = [output pal event filename]
+    name = [name of the calendar]
+    shorthand = [2-character shortcode]
--- ical2paleventfile.py
+++ ical2paleventfile.py
@@ -1,39 +1,54 @@
 #!/usr/bin/python 
 
 from ics import Calendar
-from urllib2 import urlopen # import requests
-url = '[your URL]'
-output = '[your homedir]/.pal/google.pal'
-c = Calendar(urlopen(url).read().decode('utf-8'))
+from os.path import expanduser
+from urllib2 import urlopen
+from ConfigParser import SafeConfigParser
 
-# could also use 'requests' here
-# c = Calendar(requests.get(url).text)
+homedir = expanduser("~")
 
-f = open(output, 'w')
-f.write("G1 Google Kalender \n")
+config = homedir+'/.ical2paleventfile/calendars'
 
-errorcounter = 0
+parser = SafeConfigParser()
+parser.read(config)
 
-for event in c.events:
-
-	try:
-		name = event.name
-		if (name.isspace() or len(name) == 0):
-			name = "[Event without title]"
-
-		beginstring = str(event.begin).replace('-','')[:8]
-		endstring = beginstring
-		if (event.has_end):
-			endstring = str(event.end).replace('-', '')[:8]
+for section in parser.sections():
 	
-		if (beginstring == endstring):
-			f.write(beginstring+" "+ name+"\n")
-		else:
-			f.write("DAILY:"+beginstring+":"+endstring+" "+name+"\n")
+	url = parser.get(section, "url")
+	output = parser.get(section, "palname")
+	calendarname = parser.get(section, "shorthand")+" "+parser.get(section, "name")
 
-	except UnicodeEncodeError:
-		errorcounter = errorcounter+1
-		print "Bloop "+str(errorcounter)
+	print "url: "+url+"\n"
+	print "pal: "+output+"\n"
+	print "calendar name: "+calendarname+"\n";
 
-f.close()
+	c = Calendar(urlopen(url).read().decode('utf-8'))
+
+	f = open(output, 'w')
+	f.write(calendarname+"\n")
+
+	errorcounter = 0
+
+	for event in c.events:
+
+		try:
+			name = event.name
+			if (name.isspace() or len(name) == 0):
+				name = "[Event without title]"
+
+			beginstring = str(event.begin).replace('-','')[:8]
+			endstring = beginstring
+			if (event.has_end):
+				endstring = str(event.end).replace('-', '')[:8]
+	
+			if (beginstring == endstring):
+				f.write(beginstring+" "+ name+"\n")
+			else:
+				f.write("DAILY:"+beginstring+":"+endstring+" "+name+"\n")
+
+		except UnicodeEncodeError:
+			errorcounter = errorcounter+1
+			print "Bloop "+str(errorcounter)
+
+	f.close()
 
